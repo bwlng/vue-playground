f3321c75af6b9bd748e7a600a15789c1a28385d4
Stephen Bowling <stephen@studiosaurus.com>
2019-03-27 20:44:13 -0500

    Adding main nav

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 import Vue from 'vue'
-import App from './App.vue'
+import HelloWorld from './components/HelloWorld.vue'
+import MainNav from './components/MainNav.vue'
 
 Vue.config.productionTip = false
 
+import "./styles.css";
+
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  el: "#app",
+  components: {
+    HelloWorld,
+    MainNav
+  }
+})
